ef00b8dd0b749386c8dc80ef596ac68cb77f1ab0
Rohan Mundhey <mundheyrohan@gmail.com>
2016-02-15 13:36:16 +0530

    SD card + Sensor + SMS
    all the three modules are combined & they are working synchronously

--- OSource/Final Year Project/sendsms_sketch/sendsms_sketch.ino
+++ OSource/Final Year Project/sendsms_sketch/sendsms_sketch.ino
@@ -6,23 +6,22 @@
 #include <Temboo.h>
 #include "TembooAccount.h" // Contains Temboo account information
 
+#include <SD.h>
+
+int MAX=110;
+const int chipSelect = 4;
 byte ethernetMACAddress[] = ETHERNET_SHIELD_MAC;
 EthernetClient client;
-
 int numRuns = 1;   // Execution count, so this doesn't run forever
 int maxRuns = 10;   // Maximum number of times the Choreo should be executed
 float tempC;
 int reading;
 int tempPin = 2;
-//analogReference(INTERNAL);
+
 
 
 void setup() {
-  
-
-
-  
-  
+    
   Serial.begin(9600);
   
   // For debugging, wait until the serial console is connected
@@ -37,15 +36,76 @@ void setup() {
   Serial.println("OK");
   delay(5000);
 
+
+
+
+
+
+ Serial.print("Initializing SD card...");
+
+  // see if the card is present and can be initialized:
+  if (!SD.begin(chipSelect)) {
+    Serial.println("Card failed, or not present");
+    // don't do anything more:
+    return;
+  }
+  Serial.println("card initialized.");
+
+  delay(3000);
+
+
+
   Serial.println("Setup complete.\n");
 }
 
 void loop() {
   
-  reading = analogRead(tempPin);
-  tempC = reading/9.31;
+  int k=0;
+  int i=0;
+  float final;
+  int g=0;
+  
+   String dataString = "";
+   
+   
+     while(k<10)
+  {
+    
+   reading = analogRead(tempPin);
+   delay(1000);
+   k++;
+   i=i+reading;
+}
+
+  final=i/(10*4.5);
+
+   Serial.print("Temperature value = ");
+   dataString += String(final);
+   Serial.print("\n");  
   
   
+  // open the file. note that only one file can be open at a time,
+  // so you have to close this one before opening another.
+  File dataFile = SD.open("datalog.txt", FILE_WRITE);
+  
+  
+  // if the file is available, write to it:
+  if (dataFile) {
+    dataFile.println(dataString);
+    dataFile.close();
+    // print to the serial port too:
+    Serial.println(dataString);
+  }
+  // if the file isn't open, pop up an error:
+  else {
+    Serial.println("error opening datalog.txt");
+  }
+  
+  
+  
+  if(final>MAX)
+  {
+  Serial.println(" Temperature value exceeded Normal Value \n");
   if (numRuns <= maxRuns) {
     Serial.println("Running SendSMS - Run #" + String(numRuns++));
 
@@ -66,7 +126,7 @@ void loop() {
     SendSMSChoreo.setChoreo("/Library/Twilio/SMSMessages/SendSMS");
 
     // Run the Choreo; when results are available, print them to serial
-    String BodyValue = "The temperature is " + String(tempC);
+    String BodyValue = "The temperature is " + String(final);
     SendSMSChoreo.addInput("Body", BodyValue);
   
   
@@ -82,4 +142,7 @@ void loop() {
 
   Serial.println("\nWaiting...\n");
   delay(30000); // wait 30 seconds between SendSMS calls
+  
+  }
+  
 }
